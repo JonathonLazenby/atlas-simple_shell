a825dce2c5d66af3c56f68a665c50ef71c923bf3
JonathonLazenby <lazenbyjonathon@gmail.com>
2024-03-26 15:25:00 +0000

    set up

---
+++ shell.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAX_COMMAND_LENGTH 100
+
+void display_prompt(void)
+{
+	printf("$ ");
+	fflush(stdout);
+}
+
+int main(void)
+{
+	char command[MAX_COMMAND_LENGTH];
+
+	while (1)
+	{
+		display_prompt();
+
+		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
+		{
+			printf("\nExiting shell.\n");
+			break;
+		}
+		command[strcspn(command, "\n")] = '\0';
+
+		pid_t pid = fork();
+		if (pid < 0)
+		{
+			perror("fork");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid == 0)
+        	{
+            		execlp(command, command, NULL);
+			fprintf(stderr, "Command '%s' not found\n", command);
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+		int status;
+		waitpid(pid, &status, 0);
+		}
+	}
+	return 0;
+}
